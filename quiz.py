b684862bdbf860d0b495f1350bc1cd44df79f5fb
EagleWuDev <eaglewu014@gmail.com>
2017-03-21 10:27:02 -0400

    fixes

--- quiz.py
+++ quiz.py
@@ -52,12 +52,15 @@ years (for example, 2000).
 
 
 def leap_year(year):
+if year > 1582:
     if year%4 == 0:
     	return True
-    elif year%400 == 0:
+    elif year%100 == 0 and year%400 == 0:
     	return True
     else:
     	return False
+else:
+	return False
 
 
 # When you've completed your function, uncomment the
